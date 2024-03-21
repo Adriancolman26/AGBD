d86a4792855e136241eb939b726df999292ec649
Adrian Colman <adriangamarra093@gmail.com>
2024-03-21 12:10:59 -0300

    sentencias juegos sql island

---
+++ sentencia.sql
@@ -0,0 +1,7 @@
+select * from inhabitant
+where state="friendly"
+and job = "weaponsmith"
+where job LIKE "%smith" and state ="friendly"
+select personid from inhabitant where name = "Stranger"
+select gold from inhabitant where name = "Stranger"
+select * from item where owner is null
