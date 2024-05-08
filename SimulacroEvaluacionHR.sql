1098d5ab0e2524d5e02e348ffd68bf0223d80e21
adriancolman <adriangamarra093@gmail.com>
2024-05-08 11:51:56 -0300

    Ejercicio 4 completo

--- SimulacroEvaluacionHR.sql
+++ SimulacroEvaluacionHR.sql
@@ -13,3 +13,11 @@
 3 - SELECT DISTINCT job_title, max_salary, min_salary FROM jobs  
     WHERE job_title LIKE "%manager"    
     ORDER BY max_salary DESC, min_salary DESC  
+
+-- Ejercicio 4 
+
+4 - SELECT c.country_name, r.region_name  FROM countries c
+    JOIN regions r on r.region_id = c.region_id
+    ORDER BY region_name ASC
+
+5
