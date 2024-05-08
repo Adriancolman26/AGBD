3bca65d9d07a06df034f5af07f0714297a98ae37
adriancolman <adriangamarra093@gmail.com>
2024-05-08 11:21:40 -0300

    Ejercicio 2 completo

--- SimulacroEvaluacionHR.sql
+++ SimulacroEvaluacionHR.sql
@@ -1,4 +1,11 @@
 -- Ejercicio 1  
 
-1 - SELECT DISTINCT department_name from departments         
-    ORDER BY department_name ASC
+1 -  SELECT department_name FROM departments         
+     ORDER BY department_name ASC
+
+-- Ejercicio 2
+
+2 - SELECT DISTINCT salary FROM employees         
+    ORDER BY salary DESC.
+
+-- Ejercicio 3
